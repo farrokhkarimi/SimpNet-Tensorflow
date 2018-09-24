853bfa24c65cc7897bf7e331bad025fae1df6319
Ali Gholami <aligholami7596@gmail.com>
2018-09-24 18:13:58 +0330

    Update: Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simpnet',
+      version='0.1',
+      description='A Tensorflow Implementation of the SimpNet Convolutional Neural Network Architecture',
+      url='http://github.com/hexpheus/SimpNet-Tensorflow',
+      author='Ali Gholami',
+      author_email='hexpheus@gmail.com',
+      license='MIT',
+      packages=['simpnet'],
+      zip_safe=False)
