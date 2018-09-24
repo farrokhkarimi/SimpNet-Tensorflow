05c8e56ea40a9655d1283fe338eee3fa27e80a84
Ali Gholami <aligholami7596@gmail.com>
2018-09-24 18:31:30 +0330

    Update: Added requirement grabber for setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+def get_requirements():
+      reqs = open('requirements.txt').read()
+      return [req.split() for req in reqs if req != ""]
+
 setup(name='simpnet',
       version='0.1',
       description='A Tensorflow Implementation of the SimpNet Convolutional Neural Network Architecture',
       url='http://github.com/hexpheus/SimpNet-Tensorflow',
       author='Ali Gholami',
       author_email='hexpheus@gmail.com',
+      install_requires=get_requirements(),
       license='MIT',
       packages=['simpnet'],
       zip_safe=False)
