a9d3e3e8e80c9c96f4004c911eae3950c9433c91
Ali Gholami <aligholamee@aut.ac.ir>
2018-10-25 23:24:00 +0330

    Update: Fix shape acquisition

--- simpnet/simpnet.py
+++ simpnet/simpnet.py
@@ -30,7 +30,7 @@ class SimpNet(object):
         self.learning_rate = 0.001
 
         # Setup the data path (folder should contain train and test folders inside itself)
-        self.data_path = '../images/images'
+        self.data_path = '../images/'
         self.main_csv = '../Data_Entry_2017.csv'
         self.train_val_csv = shuffle_csv('../train_val_list.csv')
         self.test_csv = shuffle_csv('../test_list.csv')
@@ -114,56 +114,56 @@ class SimpNet(object):
 
 
     def plot_confusion_matrix(correct_labels, predict_labels, labels, title='Confusion matrix', tensor_name = 'MyFigure/image', normalize=False):
-    ''' 
-    Parameters:
-        correct_labels                  : These are your true classification categories.
-        predict_labels                  : These are you predicted classification categories
-        labels                          : This is a lit of labels which will be used to display the axix labels
-        title='Confusion matrix'        : Title for your matrix
-        tensor_name = 'MyFigure/image'  : Name for the output summay tensor
+        ''' 
+        Parameters:
+            correct_labels                  : These are your true classification categories.
+            predict_labels                  : These are you predicted classification categories
+            labels                          : This is a lit of labels which will be used to display the axix labels
+            title='Confusion matrix'        : Title for your matrix
+            tensor_name = 'MyFigure/image'  : Name for the output summay tensor
 
-    Returns:
-        summary: TensorFlow summary 
+        Returns:
+            summary: TensorFlow summary 
 
-    Other itema to note:
-        - Depending on the number of category and the data , you may have to modify the figzie, font sizes etc. 
-        - Currently, some of the ticks dont line up due to rotations.
-    '''
-    cm = confusion_matrix(correct_labels, predict_labels, labels=labels)
-    if normalize:
-        cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
-        cm = np.nan_to_num(cm, copy=True)
-        cm = cm.astype('int')
+        Other itema to note:
+            - Depending on the number of category and the data , you may have to modify the figzie, font sizes etc. 
+            - Currently, some of the ticks dont line up due to rotations.
+        '''
+        cm = confusion_matrix(correct_labels, predict_labels, labels=labels)
+        if normalize:
+            cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
+            cm = np.nan_to_num(cm, copy=True)
+            cm = cm.astype('int')
 
-    np.set_printoptions(precision=2)
-    ###fig, ax = matplotlib.figure.Figure()
+        np.set_printoptions(precision=2)
+        ###fig, ax = matplotlib.figure.Figure()
 
-    fig = matplotlib.figure.Figure(figsize=(7, 7), dpi=320, facecolor='w', edgecolor='k')
-    ax = fig.add_subplot(1, 1, 1)
-    im = ax.imshow(cm, cmap='Oranges')
+        fig = matplotlib.figure.Figure(figsize=(7, 7), dpi=320, facecolor='w', edgecolor='k')
+        ax = fig.add_subplot(1, 1, 1)
+        im = ax.imshow(cm, cmap='Oranges')
 
-    classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
-    classes = ['\n'.join(wrap(l, 40)) for l in classes]
+        classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
+        classes = ['\n'.join(wrap(l, 40)) for l in classes]
 
-    tick_marks = np.arange(len(classes))
+        tick_marks = np.arange(len(classes))
 
-    ax.set_xlabel('Predicted', fontsize=7)
-    ax.set_xticks(tick_marks)
-    c = ax.set_xticklabels(classes, fontsize=4, rotation=-90,  ha='center')
-    ax.xaxis.set_label_position('bottom')
-    ax.xaxis.tick_bottom()
+        ax.set_xlabel('Predicted', fontsize=7)
+        ax.set_xticks(tick_marks)
+        c = ax.set_xticklabels(classes, fontsize=4, rotation=-90,  ha='center')
+        ax.xaxis.set_label_position('bottom')
+        ax.xaxis.tick_bottom()
 
-    ax.set_ylabel('True Label', fontsize=7)
-    ax.set_yticks(tick_marks)
-    ax.set_yticklabels(classes, fontsize=4, va ='center')
-    ax.yaxis.set_label_position('left')
-    ax.yaxis.tick_left()
+        ax.set_ylabel('True Label', fontsize=7)
+        ax.set_yticks(tick_marks)
+        ax.set_yticklabels(classes, fontsize=4, va ='center')
+        ax.yaxis.set_label_position('left')
+        ax.yaxis.tick_left()
 
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        ax.text(j, i, format(cm[i, j], 'd') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "black")
-    fig.set_tight_layout(True)
-    summary = tfplot.figure.to_summary(fig, tag=tensor_name)
-    return summary
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            ax.text(j, i, format(cm[i, j], 'd') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "black")
+        fig.set_tight_layout(True)
+        summary = tfplot.figure.to_summary(fig, tag=tensor_name)
+        return summary
 
 
     def build_network_graph(self):
@@ -377,9 +377,9 @@ class SimpNet(object):
             ground_truth  = tf.equal(self.label, tf.ones(shape=tf.shape(preds)))
 
             # Compare with 0.5 elementwise
-            target = tf.fill(preds.get_shape(), 0.5, name='target')
-            val0 = tf.fill(preds.get_shape(), 0.0, name='val0')
-            val1 = tf.fill(preds.get_shape(), 1.0, name='val1')
+            target = tf.fill(tf.shape(preds), 0.5, name='target')
+            val0 = tf.fill(tf.shape(preds), 0.0, name='val0')
+            val1 = tf.fill(tf.shape(preds), 1.0, name='val1')
             cond = tf.less(preds, target)
             preds = tf.select(cond, val0, val1)
             
